ae1c49766215e3a8f1f33caad5d30c5e3eb66208
zonghuaj <8163944@qq.com>
2020-06-18 18:53:29 +0800

    feat: add number

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -56,7 +56,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 50000,
+          limit: 5000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
